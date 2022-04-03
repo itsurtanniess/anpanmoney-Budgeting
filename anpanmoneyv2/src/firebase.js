625ab49e11b25d1fc330324da8f76ccfaf2d98c4
krminantonio <antonio.karminida@yahoo.com>
2022-04-03 20:44:25 +0800

    index

--- anpanmoneyv2/src/firebase.js
+++ anpanmoneyv2/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app"
 import  "firebase/compat/auth"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_BUCKET,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCpgsjZ8EuVpVod4_s8nOjet2TyB1hEmiE",
+  authDomain: "anpanmoney-cf5d0.firebaseapp.com",
+  databaseURL: "https://anpanmoney-cf5d0-default-rtdb.firebaseio.com",
+  projectId: "anpanmoney-cf5d0",
+  storageBucket: "anpanmoney-cf5d0.appspot.com",
+  messagingSenderId: "28971312083",
+  appId: "1:28971312083:web:22ba4e81022e45f69ac526"
 })
 
 export const auth= app.auth()
