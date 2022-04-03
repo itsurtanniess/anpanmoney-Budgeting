5b4e583bac4e582a08c0711aaae2573ca1b795a6
krminantonio <antonio.karminida@yahoo.com>
2022-04-03 22:55:42 +0800

    lol

--- anpanmoneyv2/src/index.js
+++ anpanmoneyv2/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import  ReactDOM from "react-dom/client";
-import * as ReactDOMClient from 'react-dom/client';
+ import  ReactDOM from "react-dom/client";
+//import * as ReactDOMClient from 'react-dom/client';
 import App from "./components/App";
 
 // ReactDOM.render(
@@ -9,15 +9,15 @@ import App from "./components/App";
   
 //   )
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-
-const container = document.getElementById('root');
-
-// Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the root.
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+// const container = document.getElementById('root');
+
+// // Create a root.
+// const root = ReactDOMClient.createRoot(container);
+
+// // Initial render: Render an element to the root.
+// root.render(<App />);
 
 
